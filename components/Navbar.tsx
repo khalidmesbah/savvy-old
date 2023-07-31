006619a58e2e6b6bdefbace5b7f1f7a446c926f2
khalidmesbah <swekhalidmesbah6@gmail.com>
2023-07-31 07:39:50 +0300

    add the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,65 @@
 "use client"
 
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import NextLink from "next/link"
 import { usePathname } from 'next/navigation'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function Navbar() {
+  const [width, setWidth] = useState<number>(0)
+  const [left, setLeft] = useState<number>(0)
+  const tabsContainer = useRef<HTMLDivElement>(null)
+
+
+  const tabsContainerHandler = (e: MouseEvent) => {
+    const oldTab = tabsContainer.current?.querySelector("[aria-selected='true']") as unknown as HTMLAnchorElement
+    const newTab = e.target as unknown as HTMLAnchorElement;
+
+    if (oldTab === newTab)
+      return
+
+    moveIndicator(oldTab, newTab)
+  }
+
+  function moveIndicator(oldTab: HTMLAnchorElement, newTab: HTMLAnchorElement) {
+    const newTabPosition = oldTab.compareDocumentPosition(newTab);
+    const newTabWidth = newTab.offsetWidth / tabsContainer.current?.offsetWidth;
+    let transitionWidth;
+
+    // if the new tab is to the right
+    if (newTabPosition === 4) {
+      transitionWidth =
+        newTab.offsetLeft + newTab.offsetWidth - oldTab.offsetLeft;
+    } else {
+      // if the tab is to the left
+      transitionWidth =
+        oldTab.offsetLeft + oldTab.offsetWidth - newTab.offsetLeft;
+      tabsContainer.current?.style.setProperty("--_left", newTab.offsetLeft + "px");
+    }
+
+    tabsContainer.current?.style.setProperty(
+      "--_width",
+      transitionWidth / tabsContainer.current.offsetWidth
+    );
+
+    setTimeout(() => {
+      tabsContainer.current?.style.setProperty("--_left", newTab.offsetLeft + "px");
+      tabsContainer.current?.style.setProperty("--_width", newTabWidth);
+    }, 220);
+  }
+
+  useEffect(() => {
+    const current = tabsContainer.current;
+    current?.addEventListener("click", tabsContainerHandler)
+    return () => {
+      current?.removeEventListener("click", tabsContainerHandler)
+    }
+  }, [])
+
   return (
-    <div className="bg-red-500">
+    <div className="border-2 border-red-400">
       <nav className="container overflow-auto">
-        <menu role="tablist" className="">
+        <menu role="tablist" data-width={width} ref={tabsContainer} className={`relative flex w-fit overflow-hidden border-b-[1px] border-b-[hsl(0_0%_30%)] my-12 after:content-['']`}>
           <Link link={"/home"} name="home" />
           <Link link={"/tweets"} name="tweets" />
           <Link link={"/images"} name="images" />
@@ -29,13 +80,12 @@ export default function Navbar() {
   )
 }
 
+
 const Link = ({ link, name }: { link: string, name: string }) => {
   const pathname = usePathname()
   const isActive = (link === "/" && link === pathname) || (link !== "/" && pathname.startsWith(link))
 
   return (
-    <li>
-      <NextLink role="tab" href={link} className={`capitalize ${"l"}`} aria-selected={isActive} >{name}</NextLink>
-    </li>
+    <NextLink role="tab" href={link} className={"uppercase font-[500] cursor-pointer opacity-70 text-white p-[1em_2em] hover:opacity-100 aria-selected:opacity-100"} aria-selected={isActive}>{name}</NextLink>
   )
 }
